8c074e00fb328105872900579037f3822478abe3
Honey bansal <65707655+honeybansal2968@users.noreply.github.com>
2023-03-23 15:58:30 +0530

    Update login.dart

--- login.dart
+++ login.dart
@@ -1,114 +1,109 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
-class MyLogin extends StatefulWidget {
-  const MyLogin({super.key});
+void main() => runApp(MyApp());
 
-  @override
-  State<MyLogin> createState() => _MyLoginState();
-}
-
-class _MyLoginState extends State<MyLogin> {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-          image: DecorationImage(image: AssetImage('tree.png'))),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Stack(
-          children: [
-            Container(
-              padding: const EdgeInsets.only(left: 35, top: 50),
-              child: const Text(
-                "Welcome\nBack",
-                style: TextStyle(
-                    color: Color.fromARGB(255, 23, 11, 11), fontSize: 33),
-              ),
-            ),
-            SingleChildScrollView(
-              child: Container(
-                padding: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height * 0.5,
-                    right: 35,
-                    left: 35),
-                child: Column(
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                          fillColor: Colors.grey.shade100,
-                          filled: true,
-                          hintText: 'Email',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10))),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                            fillColor: Colors.grey.shade100,
-                            filled: true,
-                            hintText: 'Password',
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10)))),
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Sign In',
-                          style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.w700,
-                              color: Color.fromARGB(255, 27, 28, 32)),
-                        ),
-                        CircleAvatar(
-                          radius: 30,
-                          backgroundColor: Color.fromARGB(255, 27, 28, 32),
-                          child: IconButton(
-                              onPressed: () {},
-                              color: Colors.white,
-                              icon: Icon(Icons.arrow_forward)),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 40,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        TextButton(
-                            onPressed: () {
-                              Navigator.pushNamed(context, 'register');
-                            },
-                            child: const Text("Sign Up",
-                                style: TextStyle(
-                                    decoration: TextDecoration.underline,
-                                    fontSize: 18,
-                                    color: Color.fromARGB(255, 27, 28, 32)))),
-                        TextButton(
-                            onPressed: () {},
-                            child: const Text("Forgot Password",
-                                style: TextStyle(
-                                    decoration: TextDecoration.underline,
-                                    fontSize: 18,
-                                    color: Color.fromARGB(255, 27, 28, 32))))
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // Application name
+      title: 'Flutter Hello World',
+      // Application theme data, you can set the colors for the application as
+      // you want
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      // A widget which will be started on application startup
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  MyHomePage({super.key, required this.title});
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        // The title text which will be shown on the action bar
+        title: Text(title),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TextFormField(
+            controller: emailController,
+            decoration: InputDecoration(hintText: "Email", labelText: "Email"),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter product name';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: passwordController,
+            decoration:
+                InputDecoration(hintText: "Password", labelText: "Password"),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter product name';
+              }
+              return null;
+            },
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Forgot Password",
+                    textAlign: TextAlign.right,
+                  )),
+            ],
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(onPressed: () {}, child: Text("Login")),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.facebook)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.apple)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.abc)),
+            ],
+          ),
+          Container(
+            width: size.width,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Don't have an acount? "),
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(color: Colors.blue),
+                    ))
+              ],
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
+//
